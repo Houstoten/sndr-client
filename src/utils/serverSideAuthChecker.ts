4994e6cba2f7efc6419e5293477ca3d579a4f7ca
Ivan Husarov <ivan.husarov@gmail.com>
2021-07-31 23:09:31 +0300

    hotfix

--- src/utils/serverSideAuthChecker.ts
+++ src/utils/serverSideAuthChecker.ts
@@ -12,7 +12,7 @@ export const getServerSideAuth = (fallbackURL: string, fallbackIfNoUser: boolean
 
     client = new ApolloClient({
         //@ts-ignore
-        link: ApolloLink.from([errorLink(refreshTokenMutation), new HttpLink({ uri: 'https://api.sndr.club/graphql', headers: context?.req?.headers })]),
+        link: new HttpLink({ uri: 'https://api.sndr.club/graphql', headers: context?.req?.headers }),
         cache: new InMemoryCache()
     })
 
